df5d58f433601b02f7986849f8f6038e5bd52040
raulnoering <raul_m_n@hotmail.com>
2022-09-23 00:20:27 -0300

    Init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,24 @@ export class AppComponent {
   title = 'bat';
   constructor(private router: Router){}
   goToIndex() {
+    this.setActive('index');
     this.router.navigate(['/', 'index']);
+    ;
   }
   goToAboutUs() {
+    this.setActive('about');
     this.router.navigate(['/', 'about']);
+  }
+
+  setActive (ide: string) {
+    var elementos = document.getElementsByClassName('nav-link');
+    for (let index = 0; index < elementos.length; index++) {
+      const element = elementos[index];
+      element.classList.remove("active");
+      
+    }
+    var element=document.getElementById(ide);
+    element?.classList.add('active');
   }
   
 }
